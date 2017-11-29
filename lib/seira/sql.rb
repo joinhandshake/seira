4e8276107241f1c9c91dac9afbe529bfde3c1c04
Josh Warfield <josh@joinhandshake.com>
2017-11-29 13:38:43 -0700

    Fill some more in

--- lib/seira/sql.rb
+++ lib/seira/sql.rb
@@ -27,11 +27,51 @@ module Seira
     private
 
     def run_create
+      version = 'POSTGRES_9_6'
+      cpu = 1
+      memory = 4
+      storage = 10
+
       name = "#{app}-#{Seira::Random.unique_name}"
+
       command = "gcloud sql instances create #{name}"
+
+      args.each do |arg|
+        if arg.start_with? '--version='
+          version = arg.split('=')[1]
+        elsif arg.start_with? '--cpu='
+          cpu = arg.split('=')[1]
+        elsif arg.start_with? '--memory='
+          memory = arg.split('=')[1]
+        elsif arg.start_with? '--storage='
+          storage = arg.split('=')[1]
+        elsif /^--[\w\-]+=.+$/.match? arg
+          command += " #{arg}"
+        else
+          puts "Warning: Unrecognized argument '#{arg}'"
+        end
+      end
+
+      command += " --database-version=#{version}"
+      command += " --cpu=#{cpu}"
+      command += " --memory=#{memory}"
+      command += " --storage-size=#{storage}"
+
+      if system(command)
+        puts "Successfully created sql instance #{name}"
+      else
+        puts "Failed to create sql instance"
+        exit(1)
+      end
     end
 
     def run_delete
+      name = "#{app}-#{args[0]}"
+      if system("gcloud sql instances delete #{name}")
+        puts "Successfully deleted sql instance #{name}"
+      else
+        puts "Failed to delete sql instance #{name}"
+      end
     end
 
     def run_list
