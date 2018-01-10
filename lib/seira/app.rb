15a1e90dba2e7c888658e14b07c2b8aa66eecc1d
Scott Ringwelski <sgringwe@mtu.edu>
2018-01-10 15:40:21 -0800

    Reduce temp folder giberish noise in output logs

--- lib/seira/app.rb
+++ lib/seira/app.rb
@@ -155,13 +155,13 @@ module Seira
     end
 
     def find_and_replace_revision(source:, destination:, replacement_hash:)
-      puts "Copying source yaml from #{source} to #{destination}"
+      puts "Copying source yaml from #{source} to temp folder"
       FileUtils.mkdir_p destination # Create the nested directory
       FileUtils.rm_rf("#{destination}/.", secure: true) # Clean out old files from the tmp folder
       FileUtils.copy_entry source, destination
 
       # Iterate through each yaml file and find/replace and save
-      puts "Iterating #{destination} files find/replace revision information"
+      puts "Iterating temp folder files find/replace revision information"
       Dir.foreach(destination) do |item|
         next if item == '.' || item == '..'
 
